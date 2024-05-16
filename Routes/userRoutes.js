c67ef2ac4a4d4b818236d43fa5c49d41f051a92f
Demilade Julius <demilade3chub@gmail.com>
2024-05-16 18:25:38 +0100

    paystack

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,17 +2,21 @@
 const express = require('express')
 const userRouter = express.Router()
 
+
 const {
   registerUser,
   updateUser,
   loginUser,
-  allUsers
+  allUsers, 
 } = require('../Controllers/userController')
 const verifyUser = require('../Middlewares/verifyUser')
+const payMoney = require('../Controllers/paystackController')
 
 userRouter.post('/userregister', registerUser)
 userRouter.post('/user-login', loginUser)
 userRouter.put('/user-update/:id', verifyUser, updateUser)
 userRouter.get ('/allusers', allUsers)
+userRouter.post('/make-payment', payMoney)
+
 
 module.exports = userRouter
